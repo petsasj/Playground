10ab1e1402e86b6c516d47c93e84f90d9e5ed1c2
Yiannis Petsas <petsasj@gmail.com>
2021-09-23 21:24:04 +0300

    Initial commit

---
+++ Playground.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Playground.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
